1b9345557d5208ba71fe19c021fbe0dff17257f3
ErodeesFleurs <862959461@qq.com>
2023-08-07 10:45:57 +0800

    add geo3D and manacher

--- FleursLibrary/geometrythd/linethd.hpp
+++ FleursLibrary/geometrythd/linethd.hpp
@@ -6,12 +6,12 @@
 namespace fleurs {
 
 template<typename T>
-struct line3d {
+struct Line3d {
     Point3d<T> a, b;
 
-    line3d() = default;
-    line3d(Point3d<T> a, Point3d<T> b) : a(a), b(b) {}
-    line3d(T A, T B, T C) {
+    Line3d() = default;
+    Line3d(Point3d<T> a, Point3d<T> b) : a(a), b(b) {}
+    Line3d(T A, T B, T C) {
         if (equals(A, 0)) {
             a = Point3d<T>(0, -C / B);
             b = Point3d<T>(1, -C / B);
@@ -23,25 +23,25 @@ struct line3d {
             b = Point3d<T>(1, -(A + C) / B);
         }
     }
-    line3d operator-(void) const { return line3d(-a, -b);}
-    line3d<T>& operator=(const line3d<T>& l) { a = l.a;b = l.b;return *this;}
-    bool operator==(const line3d<T>& l) const { return equals(a, l.a) && equals(b, l.b); }
-    bool operator!=(const line3d<T>& l) const { return !equals(a, l.a) || !equals(b, l.b); }
-    bool operator<(const line3d<T>& l) const { return equals(a, l.a) ? b < l.b : a < l.a; }
-    bool operator>(const line3d<T>& l) const { return equals(a, l.a) ? b > l.b : a > l.a; }
-    bool operator<=(const line3d<T>& l) const { return equals(a, l.a) ? b <= l.b : a <= l.a; }
-    bool operator>=(const line3d<T>& l) const { return equals(a, l.a) ? b >= l.b : a >= l.a; }
+    Line3d operator-(void) const { return Line3d(-a, -b);}
+    Line3d<T>& operator=(const Line3d<T>& l) { a = l.a;b = l.b;return *this;}
+    bool operator==(const Line3d<T>& l) const { return equals(a, l.a) && equals(b, l.b); }
+    bool operator!=(const Line3d<T>& l) const { return !equals(a, l.a) || !equals(b, l.b); }
+    bool operator<(const Line3d<T>& l) const { return equals(a, l.a) ? b < l.b : a < l.a; }
+    bool operator>(const Line3d<T>& l) const { return equals(a, l.a) ? b > l.b : a > l.a; }
+    bool operator<=(const Line3d<T>& l) const { return equals(a, l.a) ? b <= l.b : a <= l.a; }
+    bool operator>=(const Line3d<T>& l) const { return equals(a, l.a) ? b >= l.b : a >= l.a; }
 
     T distance(const Point3d<T>& p) const { return std::abs((b - a).cross(p - a)) / length(); }
-    T distance(const line3d<T>& l) const { return std::min({distance(l.a), distance(l.b), l.distance(a), l.distance(b)}); }
-    bool isParallel(const line3d<T>& l) const { return equals((b - a).cross(l.b - l.a), 0); }
-    bool isOrthogonal(const line3d<T>& l) const { return equals((b - a).dot(l.b - l.a), 0); }
-    bool isIntersect(const line3d<T>& l) const { return !is_parallel(l) || equals(distance(l), 0); }
+    T distance(const Line3d<T>& l) const { return std::min({distance(l.a), distance(l.b), l.distance(a), l.distance(b)}); }
+    bool isParallel(const Line3d<T>& l) const { return equals((b - a).cross(l.b - l.a), 0); }
+    bool isOrthogonal(const Line3d<T>& l) const { return equals((b - a).dot(l.b - l.a), 0); }
+    bool isIntersect(const Line3d<T>& l) const { return !is_parallel(l) || equals(distance(l), 0); }
     bool isIntersect(const Point3d<T>& p) const { return equals(distance(p), 0); }
 
     Point3d<T> projection(const Point3d<T>& p) const { return a + (b - a).dot(p - a) / length() * (b - a); }
     Point3d<T> reflection(const Point3d<T>& p) const { return 2 * projection(p) - p; }
-    Point3d<T> intersection(const line3d<T>& l) const {
+    Point3d<T> intersection(const Line3d<T>& l) const {
         if (isParallel(l)) return Point3d<T>(INF, INF);
         const Point3d<T> p = l.a, q = l.b;
         const T num = (q - p).cross(p - a);
--- FleursLibrary/geometrythd/segmentthd.hpp
+++ FleursLibrary/geometrythd/segmentthd.hpp
@@ -33,10 +33,48 @@ struct segment3d {
     bool operator<=(const segment3d<T>& l) const { return a <= l.a || (a == l.a && b <= l.b); }
     bool operator>=(const segment3d<T>& l) const { return a >= l.a || (a == l.a && b >= l.b); }
     T length(void) const { return (b - a).length();}
+    bool isOrthogonal(const segment3d<T>& l) const { return equals(dot(*this, l), 0.0); }
+    bool isParallel(const segment3d<T>& l) const { return equals(cross(*this, l), 0.0); }
+    bool intersect(const segment3d<T>& l) const {
+        if (std::max(a.x, b.x) < std::min(l.a.x, l.b.x) ||
+            std::max(l.a.x, l.b.x) < std::min(a.x, b.x) ||
+            std::max(a.y, b.y) < std::min(l.a.y, l.b.y) ||
+            std::max(l.a.y, l.b.y) < std::min(a.y, b.y) ||
+            std::max(a.z, b.z) < std::min(l.a.z, l.b.z) ||
+            std::max(l.a.z, l.b.z) < std::min(a.z, b.z)) return false;
+    }
+    bool intersect(const Line<T>& l) const {
+        return !equals(cross(b - a, l.b - l.a), 0.0);
+    }
+    bool intersect(const Point3d<T>& p) const {
+        return equals(cross(b - a, p - a), 0.0);
+    }
+    Point3d<T> crosspoint(const segment3d<T>& l) const {
+        T A = cross(b - a, l.b - l.a);
+        T B = cross(b - a, b - l.a);
+        if (equals(A, 0) && equals(B, 0)) return l.a;
+        if (equals(A, 0)) assert(false);
+        return l.a + (l.b - l.a) * (B / A);
+    }
+    Point3d<T> crosspoint(const Line<T>& l) const {
+        T A = cross(b - a, l.b - l.a);
+        T B = cross(b - a, b - l.a);
+        if (equals(A, 0) && equals(B, 0)) return l.a;
+        if (equals(A, 0)) assert(false);
+        return l.a + (l.b - l.a) * (B / A);
+    }
+    Point3d<T> crosspoint(const Point3d<T>& p) const {
+        assert(intersect(p));
+        return p;
+    }
     T distance(const Point3d<T>& p) const {
         if (dot(b - a, p - a) < 0) return (p - a).length();
         if (dot(a - b, p - b) < 0) return (p - b).length();
         return distance(p, *this);
+    }
+    T distance(const Line<T>& l) const {
+        if (intersect(l)) return 0;
+        return std::min(distance(l.a), distance(l.b));
     }
     Point3d<T> projection(const Point3d<T>& p) const {
         Point3d<T> base = b - a;
